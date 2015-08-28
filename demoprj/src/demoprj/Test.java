988cc39f9ebe3a40695550cd330af17a1bd2dcc6
Abhijit Kumar <abhijit.kumar@perspicanetworks.com>
2015-08-28 19:42:17 +0530

    test checkin

--- demoprj/src/demoprj/Test.java
+++ demoprj/src/demoprj/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main (String[] args){
 		System.out.println("Hello git");
 		System.out.println("Done with first checkin");
+		System.out.println("Repository created");
 	}
 
 }
